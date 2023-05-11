fae8e843ca2370ac9858cf4abfce659008bf0cfd
João Lucas <joaolucas.power@gmail.com>
2023-05-10 23:04:38 -0300

    feat: Register user function has been added

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,34 @@
+import { it, expect, describe } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+
+describe('register', () => {
+    it('should hash a user password upon registration', async () => {
+        const registerUseCase = new RegisterUseCase({
+            async findByEmail(email){
+                return null
+            },
+
+            async create(data) {
+                return {
+                    id: 'user-1',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    created_at: new Date(),
+                }
+            }
+        })
+
+
+        const {user} = await registerUseCase.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456'
+        })
+
+        const isPasswordCorrectlyHashed = await compare('123456', user.password_hash)
+
+        expect(isPasswordCorrectlyHashed).toBe(true)
+    })
+})
